d776792bf8688be2aa00a1e8a4fbdee1b67dac39
Alisson <alissonfilipe937@gmail.com>
2022-02-08 18:22:39 +0000

    Add serializers

--- server/books/serializers.py
+++ server/books/serializers.py
@@ -1,13 +1,32 @@
 from .models import Book
 from django_grpc_framework import proto_serializers
+from rest_framework import serializers
 from grpc_server.proto import books_pb2
 
 
-class BookProtoSerializer(proto_serializers.ModelProtoSerializer):
+class CreateBookProtoSerializer(proto_serializers.ModelProtoSerializer):
     class Meta:
         model = Book
         proto_class = books_pb2.BookResponse
-        fields = [
-            'id', 'author', 'country', 'language',
-            'link', 'pages', 'title', 'year',
-        ]
+        fields = '__all__'
+
+class RetrieveBookRequestSerializer(proto_serializers.ProtoSerializer):
+    book_id = serializers.IntegerField(required=True)
+
+    def validate_book_id(self, value):
+        try:
+            Book.objects.get(id=value)
+        except Book.DoesNotExist:
+            raise serializers.ValidationError("This book does not exist")
+        return value
+
+    class Meta:
+        proto_class = books_pb2.BookResponse
+
+class ListBookRequestSerializer(proto_serializers.ProtoSerializer):
+    start = serializers.IntegerField()
+    end = serializers.IntegerField()
+
+    class Meta:
+        proto_class = books_pb2.BookResponse
+
--- server/books/services.py
+++ server/books/services.py
@@ -1,22 +1,22 @@
+from email import message
 from .models import Book
 from django_grpc_framework import generics
-from .serializers import BookProtoSerializer
+from .serializers import RetrieveBookRequestSerializer, ListBookRequestSerializer, CreateBookProtoSerializer
 from django.db.models import Q
 from grpc_server.proto.books_pb2 import BookResponse
 
 
 class BookService(generics.ModelService):
     queryset = Book.objects
-    serializer_class = BookProtoSerializer
+    serializer_class = RetrieveBookRequestSerializer
+    
 
-    def ListBook(self, request, context):
-        start_year = request.start
-        end_year = request.end
-
-        if start_year and end_year:
-            filter = Q(year__gte=start_year) & Q(year__lte=end_year)
-            for book in self.queryset.filter(filter):
-                yield BookResponse(
+    def RetrieveBook(self, request, context):
+        serializer = RetrieveBookRequestSerializer(message=request)
+        if serializer.is_valid():
+            id = serializer.validated_data.get("book_id")
+            book = self.queryset.get(id=id)
+            return BookResponse(
                     book_id=book.id,
                     author=book.author,
                     country=book.country,
@@ -26,9 +26,55 @@ class BookService(generics.ModelService):
                     title=book.title,
                     year=book.year,
                 )
+        else:
+            return None
+    def ListBook(self, request, context):
+        serializer = ListBookRequestSerializer(message=request)
+        if serializer.is_valid():
+            start_year = serializer.validated_data.get("start")
+            end_year = serializer.validated_data.get("end")
+            if start_year and end_year:
+                filter = Q(year__gte=start_year) & Q(year__lte=end_year)
 
-        for book in self.queryset.all():
-            yield BookResponse(
+                for book in self.queryset.filter(filter):
+                    print(book)
+                    yield BookResponse(
+                        book_id=book.id,
+                        author=book.author,
+                        country=book.country,
+                        language=book.language,
+                        link=book.link,
+                        pages=book.pages,
+                        title=book.title,
+                        year=book.year,
+                    )
+
+            else:
+                for book in self.queryset.all():
+                    yield BookResponse(
+                        book_id=book.id,
+                        author=book.author,
+                        country=book.country,
+                        language=book.language,
+                        link=book.link,
+                        pages=book.pages,
+                        title=book.title,
+                        year=book.year,
+                    )
+            
+    def CreateBook(self, request, context):
+        serializer = CreateBookProtoSerializer(message=request)
+        if serializer.is_valid():
+            book = self.queryset.create(
+                author=serializer.validated_data.get("author"),
+                country=serializer.validated_data.get("country"),
+                language=serializer.validated_data.get("language"),
+                link=serializer.validated_data.get("link"),
+                pages=serializer.validated_data.get("pages"),
+                title=serializer.validated_data.get("title"),
+                year=serializer.validated_data.get("year"),
+            )
+            return BookResponse(
                 book_id=book.id,
                 author=book.author,
                 country=book.country,
@@ -38,38 +84,3 @@ class BookService(generics.ModelService):
                 title=book.title,
                 year=book.year,
             )
-
-    def RetrieveBook(self, request, context):
-        book = self.queryset.get(id=request.book_id)
-
-        return BookResponse(
-            book_id=book.id,
-            author=book.author,
-            country=book.country,
-            language=book.language,
-            link=book.link,
-            pages=book.pages,
-            title=book.title,
-            year=book.year,
-        )
-
-    def CreateBook(self, request, context):
-        book = self.queryset.create(
-            author=request.author,
-            country=request.country,
-            language=request.language,
-            link=request.link,
-            pages=request.pages,
-            title=request.title,
-            year=request.year,
-        )
-        return BookResponse(
-            book_id=book.id,
-            author=book.author,
-            country=book.country,
-            language=book.language,
-            link=book.link,
-            pages=book.pages,
-            title=book.title,
-            year=book.year,
-        )
