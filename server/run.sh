c90c52f4e0f02fce02bb21e35d0eb9175938e9c7
Alisson <alissonfilipe937@gmail.com>
2022-02-23 18:12:05 +0000

    update readme.md

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd $WORKDIR
-python manage.py grpcrunserver --server-key djgrpc.key --server-crt djgrpc.crt
-# python server.py
+if [ -z "$CERTIFICATE_GRPC_CRT" ] || [ -z "$CERTIFICATE_GRPC_KEY" ]
+then
+    echo Insecure channel
+    python manage.py grpcrunserver
+else
+    echo Secure channel
+    python manage.py grpcrunserver --server-key $CERTIFICATE_GRPC_KEY --server-crt $CERTIFICATE_GRPC_CRT
+fi
